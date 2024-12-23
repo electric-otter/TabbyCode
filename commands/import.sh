8534b8734901cbbed7dafcca7c3b77f29bf7d4bc
electric-otter <krw122081@gmail.com>
2024-12-23 00:19:38 +0000

    Update import.sh

--- commands/import.sh
+++ commands/import.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+    local file_path="$1"
+read import
+      cat "$file_path"
+      echo "Imported $import !"
+          else
+        echo "Error: File '$file_path' does not exist or it is corrupted." >&2
+        exit 1
+    fi
+    echo "Enter the path of the file to import:"
+read file_path
+import_file "$file_path"
